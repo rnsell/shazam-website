1e9891581bf15cdb3b50dff9eda8ef0ecdf02d1f
Robert Sell <robert.n.sell@gmail.com>
2015-11-26 20:26:21 -0500

    Added production url

--- app/lib/controller/shazam.controller.js
+++ app/lib/controller/shazam.controller.js
@@ -7,7 +7,7 @@
   var app = angular.module("shazamApp");
 
   function ShazamController($scope) {
-    var socket = io('http://localhost:8080');
+    var socket = io('http://secure-citadel-7581.herokuapp.com');
 
 
     var WEBSITEAPP = "WebsiteApp",
